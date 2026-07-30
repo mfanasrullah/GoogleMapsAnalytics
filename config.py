252271d3a0aa60ecaf4cb7f519799a01c856d112
mfanasrullah <37977282+mfanasrullah@users.noreply.github.com>
2026-07-30 15:01:55 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,3 @@
-# config.py
 import os
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
@@ -8,12 +7,10 @@ DATA_PROCESSED = os.path.join(BASE_DIR, 'data', 'processed')
 
 TARGET_LOCATIONS = {
     "Pemanasan Browser": "https://www.google.com/maps/place/Politeknik+Negeri+Batam",
-    # --- BATAM CENTRE DIPECAH JADI 2 LINK ---
     "Batam Centre Ferry Terminal (A)": "https://www.google.com/maps/place/Batam+Centre+Point+International+Ferry+Terminal/data=!4m7!3m6!1s0x31d9891d13f27f6b:0xadc0424017784eed!8m2!3d1.1307223!4d104.0551943!16s%2Fg%2F1pztqj9mj!19sChIJa3_yEx2J2TER7U54F0BCwK0?authuser=0&hl=id&rclk=1",
 
     "Batam Centre Ferry Terminal (B)": "https://www.google.com/maps/place/Batam+Center+International+Ferry+Terminal/data=!4m7!3m6!1s0x31d988fd04f61e15:0x6ded49c5814062a5!8m2!3d1.130607!4d104.0553512!16s%2Fg%2F11f1222m2p!19sChIJFR72BP2I2TERpWJAgcVJ7W0?authuser=0&hl=id&rclk=1",
 
-    # ----------------------------------------
     "Harbour Bay Ferry Terminal": "https://www.google.com/maps/place/Harbour+Bay+Ferry/data=!4m7!3m6!1s0x31d98a235c935fb5:0x1babbd4c3e3fb7df!8m2!3d1.1533325!4d103.9968005!16s%2Fg%2F1pt_822vb!19sChIJtV-TXCOK2TER37c_Pky9qxs?authuser=0&hl=id&rclk=1",
 
     "Sekupang Ferry Terminal": "https://www.google.com/maps/place/Sekupang+Ferry+Terminal/data=!4m7!3m6!1s0x31d98b3a0508aa89:0xad56350251fd4b37!8m2!3d1.125701!4d103.9265659!16s%2Fg%2F1hm3c98jb!19sChIJiaoIBTqL2TERN0v9UQI1Vq0?authuser=0&hl=en&rclk=1",
