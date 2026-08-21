7f5ca0b322f2663bacebda0c681f318202b290c9
mfanasrullah <37977282+mfanasrullah@users.noreply.github.com>
2026-08-22 06:13:59 +0700

    Update app.py

--- app.py
+++ app.py
@@ -49,7 +49,7 @@ responsive_css = """
     }
     #MainMenu {visibility: hidden;}
     footer {visibility: hidden;}
-    header {visibility: hidden;}
+    header {visibility: show;}
     [data-testid="stToolbar"] {visibility: hidden;}
     div[data-testid="metric-container"] > div > div {
         font-size: 1.6rem !important;
